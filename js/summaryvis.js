0cdff20583d571378000fb46a1615f08d8c55709
Lauren Wood <laurenwood@g.harvard.edu>
2015-04-09 11:31:47 -0400

    Updating data structure for summary

--- js/summaryvis.js
+++ js/summaryvis.js
@@ -19,12 +19,13 @@
  * @param _metaData -- the meta-data / data description object
  * @constructor
  */
-SummaryVis = function(_parentElement, _data, _event){
+SummaryVis = function(_parentElement, _data, _states,  _event){
     this.parentElement = _parentElement;
     this.data = _data;
+    this.states = _states;
     this.displayData = [];
-    this.eventHandler = _event
-
+    this.eventHandler = _event;
+    this.year = 1860;
 
     // TODO: define all constants here
     this.width = 900;
@@ -71,7 +72,7 @@ SummaryVis.prototype.initVis = function(){
 
 
     // filter, aggregate, modify data
-    this.wrangleData(null);
+    this.wrangleData(that.year);
 
     // call the update method
     this.updateVis();
@@ -82,10 +83,10 @@ SummaryVis.prototype.initVis = function(){
  * Method to wrangle the data. In this case it takes an options object
  * @param _filterFunction - a function that filters data or "null" if none
  */
-SummaryVis.prototype.wrangleData= function(_filterFunction){
+SummaryVis.prototype.wrangleData= function(year){
 
     // displayData should hold the data which is visualized
-    this.displayData = this.filterAndAggregate(_filterFunction);
+    this.displayData = this.filterAndAggregate(year);
 
     //// you might be able to pass some options,
     //// if you don't pass options -- set the default options
@@ -150,15 +151,48 @@ SummaryVis.prototype.onSelectionChange= function (selectionStart, selectionEnd){
  * @param _filter - A filter can be, e.g.,  a function that is only true for data of a given time range
  * @returns {Array|*}
  */
-SummaryVis.prototype.filterAndAggregate = function(_filter){
+SummaryVis.prototype.filterAndAggregate = function(year){
 
+    var that = this;
+    var localdata = [];
+
+    localdata["States"] = that.states.filter(function(d)
+    {
+        if(d.Year < year)
+            return true;
+    });
+    var tracks = [];
+
+
+    for(d in that.data)
+    {
+        if(d <= year)
+        {
+            //console.log(that.data[d]);
+            var tmp = [];
+            tmp["Year"] = parseInt(d);
+            if(that.data[d][0]["Tracks"] == "0")
+            {
+               tmp["start"] = 0;
+               tmp["end"] = 0;
+            }
+            else
+            {
+                tmp["start"] = that.data[d-10][0]["Tracks"];
+                tmp["end"] = that.data[d][0]["Tracks"];
+            }
+            tracks.push(tmp);
+        }
+    }
+    localdata["Tracks"] = tracks;
+    console.log(localdata); 
 
     // Set filter to a function that accepts all items
     // ONLY if the parameter _filter is NOT null use this parameter
-    var filter = function(){return true;}
+    /*var filter = function(){return true;}
     if (_filter != null){
         filter = _filter;
-    }
+    }*/
     //Dear JS hipster, a more hip variant of this construct would be:
     // var filter = _filter || function(){return true;}
 
